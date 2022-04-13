e224026183daa306b06fa119c4f901f1dfbca626
Erik Hetland <erik@zoodo.no>
2022-04-13 23:26:47 +0200

    Improve responsiveness of compo admin table

--- src/views/CompetitionAdminOverview.tsx
+++ src/views/CompetitionAdminOverview.tsx
@@ -22,60 +22,78 @@ const CompetitionAdminOverview = () => {
 
     return (
         <View>
-            <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                    <tr>
-                        <th
-                            scope="col"
-                            className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
-                        >
-                            Competition
-                        </th>
-                        <th
-                            scope="col"
-                            className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
-                        >
-                            Entries
-                        </th>
-                        <th
-                            scope="col"
-                            className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
-                        >
-                            Status
-                        </th>
-                        <th scope="col" className="relative px-6 py-3">
-                            <span className="sr-only">Edit</span>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                    {competitions.results.map((c) => (
-                        <tr key={c.id}>
-                            <td className="px-6 py-4 whitespace-nowrap">{c.name}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                {c.participant_limit ? `${c.entries_count}/${c.participant_limit}` : c.entries_count}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                                {c.published ? 'Published' : 'Not published'}
-                            </td>
-                            <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                                <Link
-                                    to={`/admin/competitions/${c.id}`}
-                                    className="p-1 ml-4 text-indigo-700 underline transition-all duration-150 rounded-sm hover:text-indigo-900 hover:bg-indigo-200"
-                                >
-                                    View
-                                </Link>
-                                <Link
-                                    to={`/admin/competitions/${c.id}/edit`}
-                                    className="p-1 ml-4 text-indigo-700 underline transition-all duration-150 rounded-sm hover:text-indigo-900 hover:bg-indigo-200"
-                                >
-                                    Edit
-                                </Link>
-                            </td>
+            <div className="mx-4 mt-8 overflow-hidden shadow ring-1 ring-black ring-opacity-5 rounded-lg">
+                <table className="min-w-full divide-y divide-gray-300">
+                    <thead className="bg-gray-50">
+                        <tr>
+                            <th
+                                scope="col"
+                                className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                            >
+                                Competition
+                            </th>
+                            <th
+                                scope="col"
+                                className="sm:hidden md:hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 table-cell"
+                            >
+                                Entries
+                            </th>
+                            <th
+                                scope="col"
+                                className="sm:hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 table-cell"
+                            >
+                                Status
+                            </th>
+                            <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                <span className="sr-only">Edit</span>
+                            </th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody className="divide-y divide-gray-200 bg-white">
+                        {competitions.results.map((c) => (
+                            <tr key={c.id}>
+                                <td className="max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 ">
+                                    {c.name}
+                                    <dl className="hidden font-normal md:block sm:block">
+                                        <dt className="sr-only">Entries</dt>
+                                        <dd className="mt-1 truncate text-gray-700">
+                                            {c.participant_limit
+                                                ? `${c.entries_count} entries (${c.participant_limit} limit)`
+                                                : `${c.entries_count} entries`}
+                                        </dd>
+                                        <dt className="sr-only hidden sm:block">Status</dt>
+                                        <dd className="hidden mt-1 truncate text-gray-500 sm:block">
+                                            {c.published ? 'Published' : 'Not published'}
+                                        </dd>
+                                    </dl>
+                                </td>
+                                <td className="sm:hidden md:hidden px-3 py-4 text-sm text-gray-700 table-cell">
+                                    {c.participant_limit
+                                        ? `${c.entries_count} (${c.participant_limit} limit)`
+                                        : c.entries_count}
+                                </td>
+                                <td className="sm:hidden px-3 py-4 text-sm text-gray-700 table-cell">
+                                    {c.published ? 'Published' : 'Not published'}
+                                </td>
+                                <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                    <Link
+                                        to={`/admin/competitions/${c.id}`}
+                                        className="text-indigo-600 hover:text-indigo-900 mr-6"
+                                    >
+                                        View<span className="sr-only">, {c.name}</span>
+                                    </Link>
+                                    <Link
+                                        to={`/admin/competitions/${c.id}/edit`}
+                                        className="text-indigo-600 hover:text-indigo-900 mr-2"
+                                    >
+                                        Edit<span className="sr-only">, {c.name}</span>
+                                    </Link>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </View>
     );
 };
