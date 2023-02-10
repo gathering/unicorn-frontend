68a393169f403bffb2634dc5e8c81116f89bea2a
Jo Emil Holen <jo-emil@outlook.com>
2023-02-10 20:27:14 +0100

    add locator

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import setupLocatorUI from '@locator/runtime';
 import { App } from './components/App';
 import { UserProvider } from './context/Auth';
 import '@reach/dialog/styles.css';
 import './index.css';
+
+if (import.meta.env.MODE === 'development') {
+    setupLocatorUI();
+}
 
 ReactDOM.render(
     <React.StrictMode>
