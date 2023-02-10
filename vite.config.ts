5239c8407a7e350a0d84c69a1cd662f04324e793
Jo Emil Holen <jo-emil@outlook.com>
2023-02-10 20:19:32 +0100

    minor upgrades and vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [reactRefresh()],
+    plugins: [
+        react({
+            babel: {
+                plugins: [
+                    [
+                        '@locator/babel-jsx/dist',
+                        {
+                            env: 'development',
+                        },
+                    ],
+                ],
+            },
+        }),
+        viteTsconfigPaths(),
+    ],
+    resolve: {
+        alias: [
+            {
+                find: /^~/,
+                replacement: '',
+            },
+        ],
+    },
+    server: {
+        port: 3000,
+    },
 });
