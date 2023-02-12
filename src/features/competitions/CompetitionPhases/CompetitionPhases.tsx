9d0fc3d6ba8ce293bac75287765a476196d9ecaf
Jo Emil Holen <jo-emil@outlook.com>
2023-02-12 14:37:01 +0100

    fix colors for competitionphases

--- src/features/competitions/CompetitionPhases/CompetitionPhases.tsx
+++ src/features/competitions/CompetitionPhases/CompetitionPhases.tsx
@@ -1,11 +1,12 @@
-import React, { useMemo } from 'react';
-import dayjs from 'dayjs';
-import isYesterday from 'dayjs/plugin/isYesterday';
-import isToday from 'dayjs/plugin/isToday';
-import isTomorrow from 'dayjs/plugin/isTomorrow';
-import type { ICompetition } from '../competition';
-import { competitionPhases } from '../../../utils/competitions';
-import './competition-phases.scss';
+import React, { useMemo } from "react";
+import dayjs from "dayjs";
+import isYesterday from "dayjs/plugin/isYesterday";
+import isToday from "dayjs/plugin/isToday";
+import isTomorrow from "dayjs/plugin/isTomorrow";
+import type { ICompetition } from "../competition";
+import { competitionPhases } from "../../../utils/competitions";
+import "./competition-phases.scss";
+import clsx from "clsx";
 
 dayjs.extend(isYesterday);
 dayjs.extend(isToday);
@@ -27,24 +28,24 @@ interface IDescriptors {
 
 const descriptors: IDescriptors = {
     registration_time: {
-        subject: 'Registration',
-        start: 'open',
-        end: 'close',
+        subject: "Registration",
+        start: "open",
+        end: "close",
     },
     run_time: {
-        subject: 'Competition',
-        start: 'start',
-        end: 'close',
+        subject: "Competition",
+        start: "start",
+        end: "close",
     },
     vote_time: {
-        subject: 'Voting',
-        start: 'open',
-        end: 'close',
+        subject: "Voting",
+        start: "open",
+        end: "close",
     },
     show_time: {
-        subject: 'Stage show',
-        start: 'start',
-        end: 'end',
+        subject: "Stage show",
+        start: "start",
+        end: "end",
     },
 };
 
@@ -82,27 +83,36 @@ const Phase = ({ phase }: { phase: [string, string, string] }) => {
 };
 
 const SubPhase = ({ heading, time, havePassed }: { heading: string; time: string; havePassed: boolean }) => {
-    const color = havePassed ? 'green-700' : 'gray-600';
+    const color = havePassed ? "green-700" : "gray-600";
 
     return (
         <section className={`flex-grow flex flex-col items-center font-light`}>
             <h2 className={`mb-4 text-${color}`}>{heading}</h2>
             <hr
-                style={{ height: '2px' }}
-                className={`border-0 bg-gray-400 -mb-3 min-w-full bg-${color} ${havePassed ? '' : 'border-dashed'}`}
+                style={{ height: "2px" }}
+                className={clsx(
+                    "border-0 bg-gray-400 -mb-3 min-w-full",
+                    {
+                        "bg-green-700": havePassed,
+                        "bg-gray-600": !havePassed,
+                    },
+                    {
+                        "border-dashed": havePassed,
+                    }
+                )}
             />
             <div className={`rounded-full bg-${color} w-6 h-6 border`} />
             <div className="flex flex-col items-center font-light">
-                <span title={dayjs(time).format('DD MMM YYYY HH:mm')}>
+                <span title={dayjs(time).format("DD MMM YYYY HH:mm")}>
                     {dayjs(time).isYesterday()
-                        ? 'Yesterday'
+                        ? "Yesterday"
                         : dayjs(time).isToday()
-                        ? 'Today'
+                        ? "Today"
                         : dayjs(time).isTomorrow()
-                        ? 'Tomorrow'
-                        : dayjs(time).format('dddd Do')}
+                        ? "Tomorrow"
+                        : dayjs(time).format("dddd Do")}
                 </span>
-                <span>{dayjs(time).format('HH:mm')}</span>
+                <span>{dayjs(time).format("HH:mm")}</span>
             </div>
         </section>
     );
